76252ba297d71926c52a2578a115b8ccf97eaf02
Bibek <bibekk.arki234@gmail.com>
2025-12-14 12:22:43 +0545

    Initialization of project

---
+++ example.cs
@@ -0,0 +1,15 @@
+using system;
+ public class hello world
+ {
+    public static void main(string[]args)
+    {
+         Console.WriteLine("hello mono world");
+         int a=5;
+         int b=8;
+         int c=a+b;
+         Console.WriteLine("the sum is {0},{1},{2}",c);
+         Console.readkey();
+    }
+    public int sum(int a,in b)
+   
+ }
